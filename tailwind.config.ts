4f5792feb3ac33704329c5d4a239090fe589c777
Christian V. Danos <cris.danos@Christians-MacBook-Pro.local>
2024-03-27 14:08:22 +0800

    update sidebar style and design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     extend: {
       colors: {
         "dark-purple" : "#081A51",
-        "light-white" : "rgba(255,255,255,0.18"
+        "light-white" : "rgba(255,255,255,0.18)"
       } 
     }
   },
