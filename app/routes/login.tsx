fb78a7e0e899950ef486ed54ace37bf7052bb4f7
june.pavia <rodney.pavia@98labs.com>
2024-04-01 17:41:22 +0800

    add login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,26 @@
+
+export default function LoginPage() {
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form className="login-form">
+        <div className="form-group">
+          Username:
+          <input
+            className="form-control"
+            type="text"
+          />
+        </div>
+        <div className="form-group">
+         Password:
+          <input
+            className="form-control"
+            type="password"
+          />
+        </div>
+        <button className="btn-login" type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
